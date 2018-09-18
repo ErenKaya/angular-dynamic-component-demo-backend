50afc3da34c8f7422f4b9feccd4b4c9e87e8f7ff
erenkaya <erenkaya1993@gmail.com>
2018-09-18 16:57:58 +0300

    axios some tests

--- front_end/indecision-app/src/playground/axios-request-test.js
+++ front_end/indecision-app/src/playground/axios-request-test.js
@@ -1,22 +1,36 @@
 import axios from 'axios';
 import React from 'react';
 
-export default class AxiosTest extends React.Component{
+export default class AxiosTest extends React.Component {
 
-    onClick =(e)=>{
+    onClick = (e) => {
+        console.log(1);
         var bodyFormData = new FormData();
-        bodyFormData.set('','');
+        bodyFormData.append('eren', 'kaya');
+        bodyFormData.append('eren1', 'kaya1');
+        let bodyJson = this.toJson(bodyFormData);
+        console.log(bodyJson);
         axios({
-            method:'get',
-            url:'component/generate'
-        }).then((success)=>{
+            method: 'get',
+            url: 'component/generate',
+            data: bodyJson,
+            headers: { 'abc': '1', 'abc1': '2' }
+        }).then((success) => {
             console.log(success);
-        }).catch((err)=>{
+        }).catch((err) => {
             console.log(err);
         })
     }
-    render(){
-        return(
+
+    toJson = (formData) => {
+        let object = {};
+        formData.forEach((value, key) => {
+            object[key] = value;
+        });
+        return JSON.stringify(object);
+    }
+    render() {
+        return (
             <div>
                 <button onClick={this.onClick}>+</button>
             </div>
